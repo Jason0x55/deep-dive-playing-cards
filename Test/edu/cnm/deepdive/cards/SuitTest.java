8d58cea7ccf6c61d4a9aba8229c1acd9112fd351
Jason Rafferty <Jason0x55@gmail.com>
2018-06-11 14:52:48 -0600

    Added test case for suit.toString().

---
+++ Test/edu/cnm/deepdive/cards/SuitTest.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.cards;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SuitTest {
+
+  @Test
+  void testToString() {
+    // TODO Test suit.toString() for expected return value.
+  }
+}
